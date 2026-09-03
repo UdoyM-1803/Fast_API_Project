6ff6ac8714c3331fbfdf59059d22a967ad8f1b50
Udoy Munna <udoymunna17@gmail.com>
2026-09-03 22:30:28 +0600

    Add load_data function and expenses.json file for expense tracking

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 from fastapi import FastAPI
+import json
 
 app = FastAPI()
 
+def load_data():
+    with open('expenses.json','r') as f:
+        data = json.load(p)
+    return data
+
 @app.get("/hello")       #Decorator
-def view():
+def hello():
     return "Hi"
 
-@app.get("/about")       #Decorator
-def view():
+@app.get("/about")       
+def about():
+    return "This is our about page."
+
+@app.get("/view")           # To read our expenses
+def view_expenses():
     return "This is our about page."
